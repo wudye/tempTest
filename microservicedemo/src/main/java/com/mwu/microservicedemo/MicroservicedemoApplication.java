21ffb8f097df36f543e0a9962453418a7542e864
wudye <mingwei.wu@hotmail.com>
2026-01-28 13:13:22 +0100

    v2

---
+++ microservicedemo/src/main/java/com/mwu/microservicedemo/MicroservicedemoApplication.java
@@ -0,0 +1,13 @@
+package com.mwu.microservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicedemoApplication.class, args);
+    }
+
+}
---
+++ microservicedemo/src/test/java/com/mwu/microservicedemo/MicroservicedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwu.microservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicedemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
